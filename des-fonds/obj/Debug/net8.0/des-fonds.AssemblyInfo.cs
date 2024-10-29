2ec69cbe7f513f749a35110e943b749b11b57397
uniADunn <uniworkdunn@gmail.com>
2024-10-29 21:35:50 +0000

    trying to commit and push files

--- des-fonds/obj/Debug/net8.0/des-fonds.AssemblyInfo.cs
+++ des-fonds/obj/Debug/net8.0/des-fonds.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("des-fonds")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967f01143a34526e2125fe048e5c3bdfea1569cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("des-fonds")]
 [assembly: System.Reflection.AssemblyTitleAttribute("des-fonds")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
