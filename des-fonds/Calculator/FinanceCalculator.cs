f82e57fe49f0bb79bd9bb52283f62ae674955811
uniADunn <uniworkdunn@gmail.com>
2024-10-31 19:26:17 +0000

    added finance caclulator class

---
+++ des-fonds/Calculator/FinanceCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace des_fonds.Calculator
+{
+    public static class FinanceCalculator
+    {
+        /// <summary>
+        /// Calculates the monthly total income
+        /// requires the month the user wants the total income from
+        /// requires the year the user wants the total income from
+        /// calculates the total
+        /// </summary>
+        /// <param name="month">the month in number representation</param>
+        /// <param name="year">the year</param>
+        /// <returns>the total income for the specified month and year for all income statements</returns>
+        public static double CalculateMonthlyIncome(int month, int year)
+        {
+            //Still to be implemented
+            return 0.00;
+        }
+        
+        public static double CalculateMonthlyExpense(int month, int year)
+        {
+            //still to be implemented
+            return 0.00;
+        }
+    }
+}
