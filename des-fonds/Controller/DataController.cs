ead95666aaf28fb13d2a22c897743b7d5ee226f0
J-M-linux <mcintoshjosh2004@gmail.com>
2024-11-18 22:21:39 +0000

    updateData

--- des-fonds/Controller/DataController.cs
+++ des-fonds/Controller/DataController.cs
@@ -10,14 +10,15 @@ namespace des_fonds.Controller
 // Basic class for connecting to database
 //database created through mySQl workbench
 
-    public class DataController
+    public static class DataController
     {
-        private MySqlConnection connection;
+        private static MySqlConnection connection;
 
-
-
-
-        public void CreateDatabase()
+        
+        
+        
+        
+        public static void CreateDatabase()
         {
             string connstring = "Data Source = localhost, Integrated Security = True, initial catalog = des_fonds;";
             string cDB = "DB";
@@ -39,6 +40,7 @@ namespace des_fonds.Controller
                     Console.WriteLine(e);
                     throw;
                 }
+
                 
             }
 
@@ -47,7 +49,7 @@ namespace des_fonds.Controller
 
 
 
-        public void Close()
+        public static void Close()
         {
             connection.Close();
             Console.WriteLine("Connection closed");
@@ -57,7 +59,7 @@ namespace des_fonds.Controller
 
 
 
-        public void createUserTable()
+        public static void createUserTable()
         {
             string utab = "CREATE TABLE user(ID PRIMARY KEY,UName varchar(100),PWD varchar(100))";
             MySqlCommand qCmd = new MySqlCommand(utab, connection);
@@ -65,68 +67,73 @@ namespace des_fonds.Controller
         }
 
         
-        public void addUserEntry(int id, string uName, string pwd)
+        public static void addUserEntry(int id, string uName, string pwd)
         {
             string insert = "INSERT INTO users(ID, UName, PWD) VALUES(id, uName, pwd)";
             MySqlCommand qCmd = new MySqlCommand(insert, connection);
             qCmd.ExecuteNonQuery();
         }
 
-        public void removeUserEntry(int id, string uName, string pwd)
+        public static void removeUserEntry(int id, string uName, string pwd)
         {
             string delete = "DELETE FROM users WHere ID = @id)";
             MySqlCommand qCmd = new MySqlCommand(delete, connection);
             qCmd.ExecuteNonQuery();
         }
         
-        public void createAddressTable()
+        public static void createAddressTable()
         {
             string atab = "CREATE TABLE address(ID PRIMARY KEY,Address varchar(100))";
             MySqlCommand qCmd = new MySqlCommand(atab, connection);
             qCmd.ExecuteNonQuery();
         }
         
-        public void addAddressEntry(int id, string address)
+        public static void addAddressEntry(int id, string address)
         {
             string insert = "INSERT INTO address(ID,Address) VALUES(id,address)";
             MySqlCommand qCmd = new MySqlCommand(insert, connection);
             qCmd.ExecuteNonQuery();
         }
 
-        public void removeAddressEntry(int id, string uName, string pwd)
+        public static void removeAddressEntry(int id, string uName, string pwd)
         {
             string delete = "DELETE FROM users WHere ID = @id)";
             MySqlCommand qCmd = new MySqlCommand(delete, connection);
             qCmd.ExecuteNonQuery();
         }
 
-        public void createIncomeTable()
+        
+        public static void createIncomeTable()
         {
             string itab = "CREATE TABLE income(source varchar(100), amount varchar(100), date datetime)";
             MySqlCommand qcmd = new MySqlCommand(itab, connection);
             qcmd.ExecuteNonQuery();
         }
         
-        public void addIncomeEntry(string source, double income, DateTime date)
+        public static void addIncomeEntry(string source, double income, DateTime date)
         {
             string insert = "INSERT INTO income(source,amount,date) VALUES(source,income,date)";
                         MySqlCommand qCmd = new MySqlCommand(insert, connection);
                         qCmd.ExecuteNonQuery();
             
         }
-        
-        public void removeIncomeEntry(int id, double expense){}
 
-        public void createExpenseTable()
+        public static void removeIncomeEntry(int id, double expense)
+        {
+            
+        }
+
+        
+        public static void createExpenseTable()
         {
             string etab = "CREATE TABLE expense(source varchar(100), amount varchar(100), date datetime)";
             MySqlCommand qcmd = new MySqlCommand(etab, connection);
             qcmd.ExecuteNonQuery();
         }
         
-        public void addExpenseEntry(int id, double expense){}
+        public static void addExpenseEntry(int id, double expense){}
         
-        public void removeExpenseEntry(int id, double expense){}
+        public static void removeExpenseEntry(int id, double expense){}
         
         
     }
