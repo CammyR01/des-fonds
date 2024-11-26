6567f969814546f94d8e4231200d63a4dbea941a
J-M-linux <mcintoshjosh2004@gmail.com>
2024-11-26 14:48:37 +0000

    messaegMEthod

--- des-fonds/Controller/DataController.cs
+++ des-fonds/Controller/DataController.cs
@@ -10,23 +10,23 @@ using System.Transactions;
 namespace des_fonds.Controller
 {
 
-// Basic class for connecting to database
-//database created through mySQl workbench
+    // Basic class for connecting to database
+    //database created through mySQl workbench
 
     public static class DataController
     {
         private static MySqlConnection connection;
 
-        
-        
-        
-        
+
+
+
+
         public static void OpenConnection()
         {
             string connString = "Server=80.195.174.63; port=3306; user=group18; password=password; database=defundsdb; sslMode=Required";
-            
+
             connection = new MySqlConnection(connString);
-            
+
             if (connection.State == ConnectionState.Closed)
             {
 
@@ -41,11 +41,11 @@ namespace des_fonds.Controller
                 }
                 catch
                 {
-                    
+
                     throw new Exception("couldnt connect");
                 }
 
-                
+
             }
 
         }
@@ -94,14 +94,14 @@ namespace des_fonds.Controller
         }
 
         public static void CreateStatementTable()
-        {  
+        {
             OpenConnection();
 
             string stab = "CREATE TABLE statements" +
                 "(ID  int PRIMARY KEY AUTO_INCREMENT," +
-                "source varchar(100) NOT NULL," + 
-                "amount varchar(100)," + 
-                "date datetime," + 
+                "source varchar(100) NOT NULL," +
+                "amount varchar(100)," +
+                "date datetime," +
                 "type ENUM('INCOME','EXPENSE') NOT NULL," +
                  "user_id INT NOT NULL," +
                 "FOREIGN KEY (user_id) REFERENCES users(ID))";
@@ -112,28 +112,28 @@ namespace des_fonds.Controller
 
         public static void CreateMessageTable()
         {
-                OpenConnection();
-                string create = "CREATE TABLE messages" +
-                "(Sender varchar(100)," +
-                "SenderID INT,"+
-                "Receiver varchar(100)," +
-                "ReceiverID INT," +
-                "Message varchar(100))";
-                MySqlCommand qCmd = new MySqlCommand(create, connection);
-                qCmd.ExecuteNonQuery();
-                 Close();
+            OpenConnection();
+            string create = "CREATE TABLE messages" +
+            "(Sender varchar(100)," +
+            "SenderID INT," +
+            "Receiver varchar(100)," +
+            "ReceiverID INT," +
+            "Message varchar(100))";
+            MySqlCommand qCmd = new MySqlCommand(create, connection);
+            qCmd.ExecuteNonQuery();
+            Close();
         }
-       // public static void CreateHousehold(string HouseHoldName) 
+        // public static void CreateHousehold(string HouseHoldName) 
         //{ OpenConnection();
-          //  string create = "CREATE TABLE @household" +
-            //   "(HouseHold_ID PRIMARY KEY NOT NULL," +
-              // "user_id" +
-              // "member_id;
-              //household id  user_id, member_id
-            //MySqlCommand qCmd = new MySqlCommand(create, connection);
-            //qCmd.Parameters.AddWithValue("@household", HouseHoldName)
-            //qCmd.ExecuteNonQuery();
-               // }
+        //  string create = "CREATE TABLE @household" +
+        //   "(HouseHold_ID PRIMARY KEY NOT NULL," +
+        // "user_id" +
+        // "member_id;
+        //household id  user_id, member_id
+        //MySqlCommand qCmd = new MySqlCommand(create, connection);
+        //qCmd.Parameters.AddWithValue("@household", HouseHoldName)
+        //qCmd.ExecuteNonQuery();
+        // }
 
         public static void AddUserEntry(string first_name, string last_name, int age, string uName, string pwd, string street, string postcode, string city, string country, out int lastId)
         {
@@ -145,7 +145,7 @@ namespace des_fonds.Controller
                 MySqlCommand qCmd = new MySqlCommand(insert, connection);
 
                 //adding parameters
-                
+
                 qCmd.Parameters.AddWithValue("@first_name", first_name);
                 qCmd.Parameters.AddWithValue("@last_name", last_name);
                 qCmd.Parameters.AddWithValue("@age", age);
@@ -174,7 +174,7 @@ namespace des_fonds.Controller
                 Close();
 
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 throw new Exception(e.Message);
             }
@@ -187,13 +187,13 @@ namespace des_fonds.Controller
             MySqlCommand qCmd = new MySqlCommand(delete, connection);
             qCmd.ExecuteNonQuery();
         }
-        public static void UpdateUserEntry(int id, string uName,string fname,string Lname)
+        public static void UpdateUserEntry(int id, string uName, string fname, string Lname)
         {
             OpenConnection();
             string update = "UPDATE users SET UName = @uName, First_Name = @fname, Last_Name = @lname WHERE ID = @id";
-            MySqlCommand qCmd = new MySqlCommand(update, connection);  
-           
-            qCmd.Parameters.AddWithValue ("@id", id);
+            MySqlCommand qCmd = new MySqlCommand(update, connection);
+
+            qCmd.Parameters.AddWithValue("@id", id);
             qCmd.Parameters.AddWithValue("@uName", uName);
             qCmd.Parameters.AddWithValue("@fname", fname);
             qCmd.Parameters.AddWithValue("@lname", Lname);
@@ -202,7 +202,7 @@ namespace des_fonds.Controller
         }
         public static User GetUserEntry(string uName)
         {
-            
+
             OpenConnection();
             string select = "SELECT u.ID, u.First_Name, u.Last_Name, u.Age, u.UName, u.PWD," +
                 " a.street, a.postcode, a.city, a.country" +
@@ -212,9 +212,9 @@ namespace des_fonds.Controller
             using (MySqlCommand command = new MySqlCommand(select, connection))
             {
                 command.Parameters.AddWithValue("@uName", uName);
-                
 
-                using(MySqlDataReader reader = command.ExecuteReader())
+
+                using (MySqlDataReader reader = command.ExecuteReader())
                 {
                     if (reader.Read())
                     {
@@ -229,8 +229,8 @@ namespace des_fonds.Controller
                         string city = reader.GetString(8);
                         string country = reader.GetString(9);
                         Close();
-                        return new User(userId, uname, pwd,fname,lname,age, street, postcode, city, country);
-                        
+                        return new User(userId, uname, pwd, fname, lname, age, street, postcode, city, country);
+
                     }
                     else
                     {
@@ -240,21 +240,21 @@ namespace des_fonds.Controller
                 }
             }
 
-            
+
         }
-        
-        
-        
-    //    public static void AddAddressEntry(int id, string address,)
-      //  {
+
+
+
+        //    public static void AddAddressEntry(int id, string address,)
+        //  {
         //    string insert = "INSERT INTO address(ID,Address) VALUES(id,address)";
-         //   MySqlCommand qCmd = new MySqlCommand(insert, connection);
-          //  qCmd.ExecuteNonQuery();
-       // }
+        //   MySqlCommand qCmd = new MySqlCommand(insert, connection);
+        //  qCmd.ExecuteNonQuery();
+        // }
 
         public static void RemoveAddressEntry(int id, string address)
         {
-            
+
             string delete = "DELETE FROM users WHere ID = @id)";
             MySqlCommand qCmd = new MySqlCommand(delete, connection);
             qCmd.ExecuteNonQuery();
@@ -272,15 +272,15 @@ namespace des_fonds.Controller
             MySqlCommand qCmd = new MySqlCommand(get, connection);
             qCmd.ExecuteNonQuery();
         }
-        
-      //  public static void CreateStatementTable()
+
+        //  public static void CreateStatementTable()
         //{
-          //  string stab = "CREATE TABLE statement(source varchar(100),amount varchar(100), date datetime,type varchar(100))";
-            //MySqlCommand qcmd = new MySqlCommand(stab, connection);
-            //qcmd.ExecuteNonQuery();
+        //  string stab = "CREATE TABLE statement(source varchar(100),amount varchar(100), date datetime,type varchar(100))";
+        //MySqlCommand qcmd = new MySqlCommand(stab, connection);
+        //qcmd.ExecuteNonQuery();
         //}
-        
-        public static void addIncomeEntry(string source,double amount, DateTime date, string type, int uid)
+
+        public static void addIncomeEntry(string source, double amount, DateTime date, string type, int uid)
         {//other way to do this is by passing income class and using getter in insert statement
             //Income income
             //income.source
@@ -288,22 +288,22 @@ namespace des_fonds.Controller
             //income.date
             OpenConnection();
             string insert = "INSERT INTO statement(source,amount,date,type,user_id) VALUES(@source,@amount,@date,@type,@uid)";
-                        MySqlCommand qCmd = new MySqlCommand(insert, connection);
-            
+            MySqlCommand qCmd = new MySqlCommand(insert, connection);
+
             qCmd.Parameters.AddWithValue("@source", source);
             qCmd.Parameters.AddWithValue("@amount", amount);
             qCmd.Parameters.AddWithValue("@date", date);
             qCmd.Parameters.AddWithValue("@type", type);
             qCmd.Parameters.AddWithValue("@uid", uid);
-            
-            
+
+
             qCmd.ExecuteNonQuery();
-            
+
         }
 
         public static void removeIncomeEntry(string source, double amount, DateTime date)
         {
-            string delete ="DELETE from statement WHERE source = @source amount = @amount date = @date type = INCOME";
+            string delete = "DELETE from statement WHERE source = @source amount = @amount date = @date type = INCOME";
             MySqlCommand qCmd = new MySqlCommand(delete, connection);
         }
 
@@ -323,53 +323,55 @@ namespace des_fonds.Controller
 
             qCmd.ExecuteNonQuery();
         }
-        
+
         public static void removeExpenseEntry(string source, double expense, DateTime date)
         {
 
         }
-        
+
         public static void GetStatementTable(DateTime date)
         {
             string get = "SELECT from statement WHERE date = @date";
-            MySqlCommand qCmd = new MySqlCommand(get,connection);
+            MySqlCommand qCmd = new MySqlCommand(get, connection);
             qCmd.ExecuteNonQuery();
         }
-        public static void GetIncomeStatements() {
+        public static void GetIncomeStatements()
+        {
             string get = "SELECT from statement WHERE type = 'INCOME';";
         }
 
         public static void GetExpenseStatements() { }
 
 
-        public static void CheckForMessage(int recId) 
+        public static void CheckForMessage(int recId)
         {
             string get = "SELECT from message WHERE ReceiverID = @recId";
-            MySqlCommand qCmd = new MySqlCommand(get,connection);
+            MySqlCommand qCmd = new MySqlCommand(get, connection);
             using (MySqlDataReader reader = qCmd.ExecuteReader())
             {
                 if (reader.Read())
                 {
-                    
+
                     string sender = reader.GetString(0);
                     int senderID = reader.GetInt32(1);
                     string Receiver = reader.GetString(3);
                     int ReceiverID = reader.GetInt32(4);
                     string message = reader.GetString(5);
 
-                    
+
                     Close();
-                    return new Message();
+                    //return new Message();
 
                 }
                 else
                 {
                     Close();
-                    throw new Exception("User not found");
+                    throw new Exception("MESSAGE CANT BE SENT!!!");
                 }
-                
+
+            }
+
         }
-         
     }
     
 }
