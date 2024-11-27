0b73c3451f55e4971bb026e39588d43d1ca0c9db
J-M-linux <mcintoshjosh2004@gmail.com>
2024-11-27 00:36:14 +0000

    finished statements page

--- des-fonds/Finances/finalstatement.cs
+++ des-fonds/Finances/finalstatement.cs
@@ -9,12 +9,14 @@ namespace des_fonds.Finances
     public class finalstatement
     {
         private List<int> statements = new List<int>();
-        private int totalStatement;
+        private int totalstatement;
+        public List<int> Statements { get => statements; set => statements = value; }
+        public int Totalstatement { get => totalstatement; set => totalstatement = value; }
 
         public finalstatement(List<int> statements, int totalStatement) 
         {
         this.statements = statements;
-        this.totalStatement = totalStatement;
+        this.totalstatement = totalStatement;
         }
     }
 }
